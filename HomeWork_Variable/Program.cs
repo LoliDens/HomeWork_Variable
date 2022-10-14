faf579515c5b9599dce514bad81f9314421d695b
LoliDens <104903662+LoliDens@users.noreply.github.com>
2022-10-14 11:19:18 +0500

    Добавьте файлы проекта.

---
+++ HomeWork_Variable/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_Variable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameFriend;
+            byte ageFriend;
+            uint iqFriend;
+            float salaryFriend;
+            bool playerHaveMoney;
+            double myIq;
+            long amountBedt;
+            char diskName;
+            short amountDay;
+            object textDialog;
+        }
+    }
+}
